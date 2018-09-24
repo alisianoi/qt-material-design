8ab393ec9e30994849284b17bd5e3ddfdf7a096d
Aleksandr Lisianoi <alisianoi@protonmail.com>
2018-09-24 15:58:27 +0200

    Add namespace mtd, use QMainWindow

--- main.cpp
+++ main.cpp
@@ -1,28 +1,46 @@
 #include "main.h"
 
-MyMainWidget::MyMainWidget(QWidget *parent) : QWidget(parent)
+namespace mtd
 {
-    auto main_lbl = new QLabel("Hello, world!");
 
-    auto cancel_btn = new QPushButton("Cancel");
-    auto accept_btn = new QPushButton("Accept");
-    auto hlayout = new QHBoxLayout();
+class CentralWidget : public QWidget
+{
+public:
+    CentralWidget(QWidget *parent=nullptr) : QWidget(parent) {
+        auto main_lbl = new QLabel("Hello, world!");
 
-    hlayout->addWidget(cancel_btn);
-    hlayout->addWidget(accept_btn);
+        auto cancel_btn = new QPushButton("Cancel");
+        auto accept_btn = new QPushButton("Accept");
+        auto hlayout = new QHBoxLayout();
 
-    auto vlayout = new QVBoxLayout();
-    vlayout->addWidget(main_lbl);
-    vlayout->addLayout(hlayout);
+        hlayout->addWidget(cancel_btn);
+        hlayout->addWidget(accept_btn);
 
-    this->setLayout(vlayout);
-}
+        auto vlayout = new QVBoxLayout();
+        vlayout->addWidget(main_lbl);
+        vlayout->addLayout(hlayout);
+
+        this->setLayout(vlayout);
+    }
+};
+
+class MainWindow : public QMainWindow
+{
+public:
+    MainWindow(QWidget *parent=nullptr) : QMainWindow(parent) {
+        this->setCentralWidget(new CentralWidget);
+    }
+};
+
+} // namespace mtd
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
-    auto window = new MyMainWidget();
+    auto window = new mtd::MainWindow();
+
     window->resize(320, 240);
     window->show();
     window->setWindowTitle("Hello, world!");
+
     return app.exec();
 }
